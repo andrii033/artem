fbf61eff3c8bda83fefa19c7f025151f4a1970b1
Andrii <ataran033@gmail.com>
2025-04-09 20:31:40 +0200

    add DataInitializer

---
+++ src/main/java/com/ta/artem/init/DataInitializer.java
@@ -0,0 +1,37 @@
+package com.ta.artem.init;
+
+import com.ta.artem.model.Role;
+import com.ta.artem.model.User;
+import com.ta.artem.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        String adminUsername = "admin";
+
+        if (userRepository.findByUsername(adminUsername) == null) {
+            User admin = new User();
+            admin.setUsername(adminUsername);
+            admin.setPassword(passwordEncoder.encode("admin"));
+            admin.setEmail("admin@example.com");
+            admin.setRole(Role.ROLE_ADMIN);
+
+            userRepository.save(admin);
+            log.info("✅ Админ создан: {}", adminUsername);
+        } else {
+            log.info("ℹ️ Админ уже существует: {}", adminUsername);
+        }
+    }
+}
